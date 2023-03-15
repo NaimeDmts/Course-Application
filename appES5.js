da24ca648c31d0843d58c237a430a00b5b501c35
NaimeDmts <naime.dmts@gmail.com>
2023-03-15 13:37:51 +0300

    4- Listeden Eleman Silme

--- appES5.js
+++ appES5.js
@@ -9,27 +9,29 @@ function UI(){
 }
 
 UI.prototype.addCourseToList = function(course){
-
     const list = document.getElementById('course-list');
-
     var html = `
         <tr>
             <td><img src="img/${course.image}"/></td>
             <td>${course.title}</td>
             <td>${course.instructor}</td>
-            <td><a href="#" class="btn btn-danger btn-sm">Delete</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">Delete</td>
 
         </tr>
     `
     list.innerHTML += html;
-
 }
 
 UI.prototype.clearControls = function(course){
     const title = document.getElementById('title').value="";
     const instructor = document.getElementById('instructor').value="";
     const image = document.getElementById('image').value="";
+}
 
+UI.prototype.deleteCourse = function(element){
+    if(element.classList.contains('delete')){
+        element.parentElement.parentElement.remove();
+    }
 }
 
 document.getElementById('new-course').addEventListener('submit', function(e){
@@ -50,4 +52,8 @@ document.getElementById('new-course').addEventListener('submit', function(e){
     //clear controls
     ui.clearControls();
     e.preventDefault();
+})
+document.getElementById('course-list').addEventListener('click', function(e){
+    const ui = new UI();
+    ui.deleteCourse(e.target);
 })
